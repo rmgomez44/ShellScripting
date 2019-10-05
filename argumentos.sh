3c2378a07c6742a9f199177237fb5822cecfe82a
Roberto Gomez <rmgomez44@gmail.com>
2019-10-05 13:05:34 -0300

    Add files via upload

---
+++ argumentos.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+if [ $# -eq 0 ]
+then
+    if [ $1 == "install" ]
+    then
+        echo "procediendo a instalar $2"
+    fi
+fi
---
+++ visual_studio_installer.sh
@@ -0,0 +1,37 @@
+#!bin/bash
+
+# Chequear usuario apropiado
+Usuario=whoami
+
+if  [$Usuario = root]
+then
+    ################################
+    #  Declaracion de variables 
+    ################################
+
+    export Software=VSCODE
+
+    ################################
+    # agregar el Repo 
+    ################################
+    echo "[$Software]
+    name=Visual Studio Code
+    baseurl=https://packages.microsoft.com/yumrepos/vscode
+    enabled=1
+    gpgcheck=1
+    gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo
+
+    # Actualzar RPM - Gestór de Paquetes
+    rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+    ## CentOS and RHEL system
+    yum check-update
+    yum install code
+else
+    echo "Se requiere ser por ROOT para ejecutar este Script ################################
+    #  Declaracion de variables 
+    ################################
+
+    export Software=VSCODE
+"
+fi
